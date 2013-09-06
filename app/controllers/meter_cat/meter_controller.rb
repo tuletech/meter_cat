2e775b30328e2ed9d42de770aeeca7c4e3bbbc20
Rich Humphrey <rdh727@gmail.com>
2013-09-06 01:00:40 -0700

    First pass at rendering

--- app/controllers/meter_cat/meter_controller.rb
+++ app/controllers/meter_cat/meter_controller.rb
@@ -1,7 +1,21 @@
-class MeterCat::MeterController < ApplicationController
+module MeterCat
 
-  def index
-    @meters = []
+  class MeterController < ApplicationController
+
+    def index
+      @start = Date.today - 7
+      @stop = Date.today
+
+      @range = @start .. @stop
+      @meters = {}
+
+      Meter.select( 'name,created_on,value' ).where( :created_on => @range ).find_each do |meter|
+        name = meter.name.to_sym
+        @meters[ name ] ||= {}
+        @meters[ name ][ meter.created_on ] = meter.value
+      end
+    end
+
   end
 
 end
